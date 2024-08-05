be0c73967bd93ff5a5cfc99c1cb3a237cf1e660d
vaporvee <yannik@vaporvee.com>
2024-08-05 02:06:37 +0200

    added docker support

--- build.py
+++ build.py
@@ -1,8 +1,15 @@
 import os
 import subprocess
+import shutil
+
+app_name = 'acecore'
 
 src_dir = './plugin_src'
-output_dir = './plugins'
+output_dir = './build'
+
+subprocess.run('go build .', shell=True, check=True)
+shutil.copy(app_name, output_dir)
+print(f'Built main executable in {output_dir}/{app_name}')
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
@@ -10,6 +17,7 @@ if not os.path.exists(output_dir):
 for folder_name in os.listdir(src_dir):
     folder_path = os.path.join(src_dir, folder_name)
     if os.path.isdir(folder_path):
-        command = f'go build -buildmode=plugin -o {output_dir}/{folder_name}.so {folder_path}'
+        command = f'go build -buildmode=plugin -o {output_dir}/plugins/{folder_name}.so {folder_path}'
         subprocess.run(command, shell=True, check=True)
         print(f'Built plugin: {folder_name}.so')
+
